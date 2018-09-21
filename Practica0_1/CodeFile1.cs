da169732c798decac9611937a0f20e3390e95705
Xuzeng Mao <xuzengmao@usal.es>
2018-09-21 20:05:21 +0200

    Initial commit

---
+++ Practica0_1/CodeFile1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace Proyectos.ElSegundo
+{
+    class Basica
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Console.WriteLine("EMPEZANDO");
+            Window win = new Window();
+            win.Title = "Segundo Programa";
+            win.Show();
+            Console.WriteLine("Ventana abierta");
+            Label etiqueta = new Label();
+            etiqueta.Content = "Hola Mundo";
+            win.Content = etiqueta;
+            Application app = new Application();
+            Console.WriteLine("Esperando que pase algo");
+            app.Run();
+            Console.WriteLine("se acabó");
+        }
+    }
+}
