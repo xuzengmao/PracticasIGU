90f79fc52796e8937da88bbe63f468a97b7faaed
Xuzeng Mao <xuzengmao@usal.es>
2018-10-20 20:15:07 +0200

    Versión: 0.12
    -> Añadido: Proyecto Practica 2_4.

---
+++ Practica2_4/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica2_4
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static float factor_ED = 1.30F;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Dolar.KeyDown += new KeyEventHandler(Euro_KeyDown);
+            Euro.KeyDown += new KeyEventHandler(Euro_KeyDown);
+        }
+
+        private void Euro_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                if (sender == Euro)
+                    Dolar.Text = ((Euro.DoubleValue * factor_ED)).ToString();
+                else if (sender == Dolar)
+                    Euro.Text = ((Dolar.DoubleValue / factor_ED)).ToString();
+        }
+    }
+}
---
+++ Practica2_4/NumericTextBox.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Practica2_4
+{
+    public class NumericTextBox : TextBox
+    {
+        public NumericTextBox ()
+        {
+            this.PreviewTextInput += new TextCompositionEventHandler(NumericTextBox_PreviewTextInput);
+        }
+
+        public int IntValue
+        {
+            get
+            {
+                return Int32.Parse(this.Text);
+            }
+        }
+
+        public double DoubleValue
+        {
+            get
+            {
+                return Double.Parse(this.Text);
+            }
+        }
+        
+        void NumericTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            NumberFormatInfo numberFormatInfo = System.Globalization.CultureInfo.CurrentCulture.NumberFormat;
+            string decimalSeparator = numberFormatInfo.NumberDecimalSeparator;
+            string negativeSign = numberFormatInfo.NegativeSign;
+            string caracter = e.Text;
+
+            if (Char.IsDigit(e.Text[0]))
+            {
+                // No hacemos nada porque aceptamos los dígitos
+            }
+            else if (caracter.Equals(decimalSeparator) || caracter.Equals(negativeSign))
+            {
+                // No hacemos nada porque aceptamos el punto y el signo
+            }
+            else if (caracter == "\b")
+            {
+                // No hacemos nada porque aceptamos el Backspace
+            } else
+            {
+                // Nos saltamos el carácter deteniendo el enrutamiento
+                e.Handled = true;
+            }
+        }
+    }
+}
