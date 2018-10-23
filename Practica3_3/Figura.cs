613c29bc631e92631ea4a328fba8af61abb391c6
Xuzeng Mao <xuzengmao@usal.es>
2018-10-23 20:14:55 +0200

    Versión: 0.14
    -> Añadido: Proyecto Practica3_3.

---
+++ Practica3_3/Figura.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Practica3_3
+{
+    class Figura
+    {
+        double x_pos, y_pos;
+        double x_des, y_des;
+        Shape figura;
+
+        public Figura(double limite_x, double limite_y)
+        {
+            Random rand = new Random();
+            x_pos = rand.Next((int)limite_x);
+            y_pos = rand.Next((int)limite_y);
+            x_des = rand.Next((int)limite_x / 20);
+            y_des = rand.Next((int)limite_y / 20);
+            int i = rand.Next(1);
+
+            if (i == 0)
+                figura = new Ellipse();
+            else
+                figura = new Rectangle();
+
+            figura.Fill = new SolidColorBrush(Color.FromArgb((byte)rand.Next(255), (byte)rand.Next(255), (byte)rand.Next(255), (byte)rand.Next(255)));
+            figura.Width = 10 + rand.Next((int)limite_y / 20);
+            figura.Height = 10 + rand.Next((int)limite_y / 20);
+        }
+
+        public void mueve(double limite_x, double limite_y)
+        {
+            if (x_pos + x_des < limite_x)
+                x_pos += x_des;
+            else
+                x_pos = x_pos + x_des - limite_x;
+
+            if (y_pos + y_des < limite_y)
+                y_pos += y_des;
+            else
+                y_pos = y_pos + x_pos - limite_y;
+        }
+
+        public double x
+        {
+            get { return x_pos; }
+        }
+
+        public double y
+        {
+            get { return y_pos; }
+        }
+
+        public Shape fig
+        {
+            get { return figura; }
+        }
+    }
+}
---
+++ Practica3_3/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Practica3_3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Collection<Figura> listafiguras = new Collection<Figura>();
+        DispatcherTimer timer;
+        bool flag = true;
+
+        public object Static { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Tick += OnTick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            foreach (Figura mifig in listafiguras)
+            {
+                mifig.mueve(Lienzo.ActualWidth, Lienzo.ActualHeight);
+                Canvas.SetLeft(mifig.fig, mifig.x);
+                Canvas.SetTop(mifig.fig, mifig.y);
+            }
+        }
+
+        private void CrearFigura_Click(object sender, RoutedEventArgs e)
+        {
+            Figura figura = new Figura(Lienzo.ActualWidth, Lienzo.ActualHeight);
+            listafiguras.Add(figura);
+            Lienzo.Children.Add(figura.fig);
+            Canvas.SetLeft(figura.fig, figura.x);
+            Canvas.SetTop(figura.fig, figura.y);
+        }
+
+        private void Animar_Click(object sender, RoutedEventArgs e)
+        {
+            if(flag)
+            {
+                timer.Start();
+                Animar.Content = "Parar";
+                flag = false;
+            }
+            else
+            {
+                timer.Stop();
+                Animar.Content = "Animar";
+                flag = true;
+            }
+        }
+    }
+}
