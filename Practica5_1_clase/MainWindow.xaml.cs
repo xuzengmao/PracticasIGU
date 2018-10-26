ba2fe24d86c84edec2027b69412fcc5426db9cc9
Xuzeng Mao <xuzengmao@usal.es>
2018-10-26 16:03:48 +0200

    Versión: 0.16
    -> Añadido: Proyecto Practica5_1_clase.

---
+++ Practica5_1_clase/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica5_1_clase
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click_Configurar(object sender, RoutedEventArgs e)
+        {
+            VentanaConfigurar cd = new VentanaConfigurar();
+            cd.Owner = this;
+            cd.HaCambiadoCadena += Cd_HaCambiadoCadena;
+            cd.textBox.Text = this.Title;
+
+            cd.Show();
+        }
+
+        private void Cd_HaCambiadoCadena(object sender, CambiosCadenaEventArgs e)
+        {
+            this.Title = e.LaCadena;
+        }
+
+        private void MenuItem_Click_Salir(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Practica5_1_clase/VentanaConfigurar.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Practica5_1_clase
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaConfiguar.xaml
+    /// </summary>
+    
+    public class CambiosCadenaEventArgs : EventArgs
+    {
+        public string LaCadena { get; set; }
+    }
+
+    public delegate void CambiosCadenaEventHandler(Object sender, CambiosCadenaEventArgs e);
+
+    public partial class VentanaConfigurar : Window
+    {
+        public event CambiosCadenaEventHandler HaCambiadoCadena;
+
+        public VentanaConfigurar()
+        {
+            InitializeComponent();
+        }
+
+        void OnHaCambiadoCadena (CambiosCadenaEventArgs e)
+        {
+            if (HaCambiadoCadena != null)
+            {
+                HaCambiadoCadena(this, e);
+            }
+        }
+
+        private void Cerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CambiosCadenaEventArgs arg = new CambiosCadenaEventArgs();
+            arg.LaCadena = textBox.Text;
+            OnHaCambiadoCadena(arg);
+        }
+    }
+}
