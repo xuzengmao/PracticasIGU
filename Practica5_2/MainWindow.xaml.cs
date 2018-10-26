29803a02ee2e7a320f16120750b1521c67ea949f
Xuzeng Mao <xuzengmao@usal.es>
2018-10-26 20:26:29 +0200

    Versión: 0.17
    -> Añadido: Proyecto Practica5_2.

---
+++ Practica5_2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica5_2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Color config_color;
+        VentanaConfigurar cd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            config_color = ((SolidColorBrush)Lienzo.Background).Color;
+        }
+
+        private void MenuItem_Click_configurar(object sender, RoutedEventArgs e)
+        {
+            cd = new VentanaConfigurar();
+            cd.Owner = this;
+            cd.C_R = config_color.R;
+            cd.C_G = config_color.G;
+            cd.C_B = config_color.B;
+            cd.CambioColor += cd_CambioColor;
+            cd.Show();
+        }
+
+        private void MenuItem_Click_salir(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Application.Current.Shutdown();
+        }
+
+        void cd_CambioColor(object sender, CambioColorEventArgs e)
+        {
+            config_color = e.ColorCambiado;
+            Brush brocha = new SolidColorBrush(config_color);
+            Lienzo.Background = brocha;
+        }
+    }
+}
---
+++ Practica5_2/VentanaConfigurar.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Practica5_2
+{
+
+    public class CambioColorEventArgs : EventArgs
+    {
+        public Color ColorCambiado;
+    }
+
+    public delegate void CambioColorEventHandler(object sender, CambioColorEventArgs e);
+
+    /// <summary>
+    /// Lógica de interacción para VentanaConfigurar.xaml
+    /// </summary>
+    public partial class VentanaConfigurar : Window
+    {
+        public event CambioColorEventHandler CambioColor;
+        Color color_admin;
+
+        public int C_R
+        {
+            get { return color_admin.R; }
+            set { color_admin.R = (byte)value; RedSlider.Value = value; }
+        }
+
+        public int C_G
+        {
+            get { return color_admin.G; }
+            set { color_admin.G = (byte)value; GreenSlider.Value = value; }
+        }
+
+        public int C_B
+        {
+            get { return color_admin.B; }
+            set { color_admin.B = (byte)value; BlueSlider.Value = value; }
+        }
+
+        public VentanaConfigurar()
+        {
+            InitializeComponent();
+        }
+
+        void OnCambioColor(CambioColorEventArgs e)
+        {
+            if (this.CambioColor != null)
+                this.CambioColor(this, e);
+        }
+
+        private void Window_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color_admin = Color.FromRgb((byte)Math.Round(RedSlider.Value, 0),
+                                        (byte)Math.Round(GreenSlider.Value),
+                                        (byte)Math.Round(BlueSlider.Value));
+
+            CambioColorEventArgs argumentos = new CambioColorEventArgs();
+            argumentos.ColorCambiado = color_admin;
+            OnCambioColor(argumentos);
+        }
+
+        private void Quit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
