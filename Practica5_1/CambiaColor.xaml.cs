5a6ef3bef0f56501f4f8c96f77d2986f41714e73
Xuzeng Mao <xuzengmao@usal.es>
2018-10-26 19:04:27 +0200

    Versión: 0.16
    -> Añadido: Proyecto Practica5_1.

---
+++ Practica5_1/CambiaColor.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Practica5_1
+{
+    /// <summary>
+    /// Lógica de interacción para CambiaColor.xaml
+    /// </summary>
+    public partial class CambiaColor : Window
+    {
+        Color color_admin;
+
+        public int C_R
+        {
+            get { return color_admin.R; }
+            set { color_admin.R = (byte)value; RedSlider.Value = value; }
+        }
+
+        public int C_G
+        {
+            get { return color_admin.G; }
+            set { color_admin.G = (byte)value; GreenSlider.Value = value; }
+        }
+
+        public int C_B
+        {
+            get { return color_admin.B; }
+            set { color_admin.B = (byte)value; BlueSlider.Value = value; }
+        }
+
+        public CambiaColor()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void Window_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color_admin = Color.FromRgb((byte)Math.Round(RedSlider.Value, 0),
+                                        (byte)Math.Round(GreenSlider.Value),
+                                        (byte)Math.Round(BlueSlider.Value));
+        }
+    }
+}
--- Practica5_1/MainWindow.xaml.cs
+++ Practica5_1/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace Practica5_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Color config_color;
+
         public MainWindow()
         {
             InitializeComponent();
+            config_color = ((SolidColorBrush)Lienzo.Background).Color;
+        }
+
+        private void MenuItem_Click_configurar(object sender, RoutedEventArgs e)
+        {
+            CambiaColor cd = new CambiaColor();
+            cd.Owner = this;
+            cd.C_R = config_color.R;
+            cd.C_G = config_color.G;
+            cd.C_B = config_color.B;
+
+            cd.ShowDialog();
+            
+            if (cd.DialogResult == true)
+            {
+                config_color.R = (byte)cd.C_R;
+                config_color.G = (byte)cd.C_G;
+                config_color.B = (byte)cd.C_B;
+            }
+
+            cd.Close();
+            Lienzo.Background = new SolidColorBrush(config_color);
+        }
+
+        private void MenuItem_Click_salir(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Application.Current.Shutdown();
         }
     }
 }
